7c34b57491541937851e8c31cf77676065c1e585
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-06 01:46:34 -0800

    solving linter errors

--- keycodes.js
+++ keycodes.js
@@ -1,6 +1,6 @@
 const insert = document.getElementById('insert')
 window.addEventListener('keydown', (e) => {
- insert.innerHTML = `
+  insert.innerHTML = `
  <div class="key">
  ${e.key === ' ' ? 'Space' : e.key}
 <small>event.key</small>
